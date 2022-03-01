409d6bb32d3ed8bb69ac177895d7a1edd6e3b189
asb1512 <andrew@olympicmedia.com>
2022-03-01 11:08:16 -0500

    Initial commit

---
+++ popup.js
@@ -0,0 +1,7 @@
+// locates extension's start button
+let startBtn = document.getElementById('start-btn');
+
+// adds event listener to start button
+startBtn.addEventListener('click', function() {
+  startBtn.style.backgroundColor = 'green';
+});
