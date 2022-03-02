c084636927b5e3a51bf55853fa0372056b5df8aa
asb1512 <andrew@olympicmedia.com>
2022-03-02 11:30:36 -0500

    removed unused code

--- popup.js
+++
@@ -1,7 +0,0 @@
-// locates extension's start button
-let startBtn = document.getElementById('start-btn');
-
-// adds event listener to start button
-startBtn.addEventListener('click', function() {
-  startBtn.style.backgroundColor = 'green';
-});
